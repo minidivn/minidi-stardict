15918672f3a68d2878cb5d2b0fe1af10e1e1c898
Cuong Nguyen Manh <atomixnmc@gmail.com>
2020-10-15 16:48:08 -0400

    Update app.js
    Change to start server without mongodb

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var path = require('path');
 var config = require('./config/index');
 
-var mongoBootstrap = require('./bootstrap/mongoose');
-mongoBootstrap.connect()
-.then((dbConfig) => {
+var startServer = (dbConfig) => {
 	var expressBootstrap = require('./bootstrap/express-server');
 	expressBootstrap.init();
 
 	var cache = require('./bootstrap/cache');
 	cache.init(expressBootstrap.app);
 
+	/*
 	if (config.seed) {
 		var seeding = require('./bootstrap/seed');
 		seeding.start(expressBootstrap.app);
 	}
-
+	*/
 	expressBootstrap.start();
-});
+}
+/*
+var mongoBootstrap = require('./bootstrap/mongoose');
+mongoBootstrap
+.connect()
+.then(startServer);
+*/
+
+startServer();
